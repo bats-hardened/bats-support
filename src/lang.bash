3f68ffb3f1e650fd1ef3b837b59f83c5b93f3e3e
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-09-12 15:48:22 +0200

    lint: lint .bash files

--- src/lang.bash
+++ src/lang.bash
@@ -39,10 +39,16 @@ batslib_is_caller() {
   local -i is_mode_direct=1
 
   # Handle options.
-  while (( $# > 0 )); do
+  while (($# > 0)); do
     case "$1" in
-      -i|--indirect) is_mode_direct=0; shift ;;
-      --) shift; break ;;
+      -i | --indirect)
+        is_mode_direct=0
+        shift
+        ;;
+      --)
+        shift
+        break
+        ;;
       *) break ;;
     esac
   done
@@ -51,11 +57,11 @@ batslib_is_caller() {
   local -r func="$1"
 
   # Check call stack.
-  if (( is_mode_direct )); then
+  if ((is_mode_direct)); then
     [[ $func == "${FUNCNAME[2]}" ]] && return 0
   else
     local -i depth
-    for (( depth=2; depth<${#FUNCNAME[@]}; ++depth )); do
+    for ((depth = 2; depth < ${#FUNCNAME[@]}; ++depth)); do
       [[ $func == "${FUNCNAME[$depth]}" ]] && return 0
     done
   fi
